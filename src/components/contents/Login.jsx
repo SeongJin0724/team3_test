50a730c32f901f4f71691f4cc662b097c0af0559
LSJ <thls702464@gmail.com>
2024-04-15 22:29:39 +0900

    🐛 api수정 server제거

--- src/components/contents/Login.jsx
+++ src/components/contents/Login.jsx
@@ -1,30 +1,45 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 export default function Login() {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-  
-    const handleSubmit = async (e) => {
-      e.preventDefault();
-      try {
-        const response = await fetch('/api/login', {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify({ email, password })
-        });
-        const data = await response.json();
-        console.log(data);
-        alert(data.message);
-      } catch (error) {
-        console.error('Error:', error);
-      }
-    };
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch(
+        `${process.env.REACT_APP_API_URL}/api/login`,
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ email, password }),
+        }
+      );
+      const data = await response.json();
+      console.log(data);
+      alert(data.message);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
   return (
     <form onSubmit={handleSubmit}>
-    <h2>로그인</h2>
-    <input type="email" name="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} required />
-    <input type="password" name="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} required />
-    <button type="submit">로그인</button>
-  </form>
-  )
+      <h2>로그인</h2>
+      <input
+        type="email"
+        name="email"
+        placeholder="Email"
+        onChange={(e) => setEmail(e.target.value)}
+        required
+      />
+      <input
+        type="password"
+        name="password"
+        placeholder="Password"
+        onChange={(e) => setPassword(e.target.value)}
+        required
+      />
+      <button type="submit">로그인</button>
+    </form>
+  );
 }
--- src/pages/Pages1.jsx
+++ src/pages/Pages1.jsx
@@ -1,24 +1,29 @@
 import React, { useEffect, useState } from "react";
 import Main from "../components/section/Main";
-import axios from 'axios';
+import axios from "axios";
 
 export default function Pages1() {
   const [data, setData] = useState([]);
 
-useEffect(() => {
-  axios.get('/product')
-  .then(response => {
-    setData(response.data)
-  })
-  .catch(error => {
-    console.error('API 호출 중 에러 발생:', error);
-  });
-},[])
-  return <Main>
- <ul>
-  {data.map((item,index) => (
-    <li key={item.formKey}>{`Name:${item.name} Img:${item.img} Description:${item.description} Price:${item.price}`}</li>
-  ))}
- </ul>
- </Main>;
+  useEffect(() => {
+    axios
+      .get(`${process.env.REACT_APP_API_URL}/api/users`)
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.error("API 호출 중 에러 발생:", error);
+      });
+  }, []);
+  return (
+    <Main>
+      <ul>
+        {data.map((item, index) => (
+          <li key={item.formKey}>
+            {`Name:${item.name} Img:${item.img} Description:${item.description} Price:${item.price} TEL:${item.tel} `}{" "}
+          </li>
+        ))}
+      </ul>
+    </Main>
+  );
 }
