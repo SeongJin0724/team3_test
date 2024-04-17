773f975eaf8f2d25c7381f36f7ca4122f0131069
jimmy <skawk3235@gmail.com>
2024-04-17 18:06:27 +0900

    🚧

--- src/components/section/Header.jsx
+++ src/components/section/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Header() {
   return (
     <div className="header">
-      <h1 className="header_h1">헤더입니다</h1>
+      <h1 className="header_h1">헤더입니다1</h1>
     </div>
   );
 }
